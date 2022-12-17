5e19bf10ce60203fa05d3eabea41c95692f51c0c
Alimi Kehinde Sherifah <101565387+abimbolasherifah@users.noreply.github.com>
2022-12-17 08:23:35 +0100

    Create 1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    return(my_list[idx])
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
